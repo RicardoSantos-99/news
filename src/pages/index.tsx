7d1d69fa34b05d76ac8109b63ce8b0468406e880
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-10-07 14:39:46 -0300

    feat: start

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <h1>
+        <Head>
+          <title>news</title>
+        </Head>
+        Hello World
+      </h1>
+    </Fragment>
+  );
+}
